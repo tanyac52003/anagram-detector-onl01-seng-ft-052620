f5519727d3b01683dca9b7cd8b7e48f752cc3d74
Tanya Hodges <tanyac52003@gmail.com>
2020-06-07 17:19:09 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(words_array)
+    words_array.select do |element|
+      (@word.split("").sort == (element.split("").sort))
+    end 
+  end 
+end 
