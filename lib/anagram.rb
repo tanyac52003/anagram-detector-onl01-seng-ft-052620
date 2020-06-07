5d246a8620cb02b63f58e8eb0670ac6516f9ecb4
Tanya Hodges <tanyac52003@gmail.com>
2020-06-07 17:18:55 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,4 +6,9 @@ class Anagram
     @word = word 
   end 
   
+  def match(words_array)
+    words_array.select do |element|
+      (@word.split("").sort == (element.split("").sort))
+    end 
+  end 
 end 
